d0da9402156020b213c8af4a6bea572f7e890965
Richard Hriech <hardric501@gmail.com>
2018-10-03 16:03:40 +0200

    Static email display

---
+++ SaintSender/SaintSender.DesktopUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SaintSender.DesktopUI.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public Object Inbox { get; set; }
+    }
+}
