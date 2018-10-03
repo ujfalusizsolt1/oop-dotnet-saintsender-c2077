5fc23b81a37eae41a4953099c3761e40c9e7ca92
ujfalusizsolt1 <ujfalusi.zsolt1@gmail.com>
2018-10-03 15:22:39 +0200

    skeleton code created for inbox

---
+++ SaintSender/SaintSender/Entities/Inbox.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SaintSender.Services;
+using SaintSender.Entities;
+
+namespace SaintSender.Entities
+{
+    class Inbox
+    {
+        public List<Email> emails;
+
+        public List<Email> GetMails()
+        {
+            /* 
+            get client
+            List<Email> result = new List<Email>;
+            var inbox = client.inbox;
+            
+            for (int i = 0; i < inbox.Count; i++)
+            {
+                result.Add(inbox.GetMessage(i));
+            }
+
+            return result;
+            */
+        }
+
+        public void OpenMail(int id)
+        {
+            /*
+            get client;
+            var inbox = client.inbox;
+            inbox.GetMessage(id);
+            */
+
+        }
+
+        public void SendMail(Email toSend)
+        {
+
+        }
+
+        public void SaveDraft()
+        {
+
+        }
+
+        private void ReOpenDraft()
+        {
+
+        }
+
+    }
+}
