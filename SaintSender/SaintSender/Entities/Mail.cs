c4b026f22cde96f13705fa1aa798eb0b6185bf6e
mezitomi <mezitomi@gmail.com>
2018-10-03 15:13:38 +0200

    Mail class added; Test for ctor added.

---
+++ SaintSender/SaintSender/Entities/Mail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SaintSender.Entities
+{
+    public class Mail
+    {
+        public int Id { get; }
+        public string Sender { get; }
+        public string Reciever { get; }
+        public DateTime Date { get; }
+        public string Subject { get; }
+        public bool IsRead { get; set; }
+        public string Content { get; }
+
+        public Mail(int id, string sender, string reciever, DateTime date, string subject, bool isRead, string content)
+        {
+            Id = id;
+            Sender = sender;
+            Reciever = reciever;
+            Date = date;
+            Subject = subject;
+            IsRead = isRead;
+            Content = content;
+        }
+    }
+}
